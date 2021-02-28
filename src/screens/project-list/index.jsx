ac4368f730977d2e61448b418313bed746edef70
Chenlei Fu <chenlei.fu@outlook.com>
2021-02-28 14:41:47 -0600

    feat: add two custome hooks: useMount and useDebounce

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,13 +1,11 @@
 import { SearchPanel } from "./search-panel";
 import { List } from "./list";
 import { useEffect, useState } from "react";
-import { cleanObject } from "../../utils";
+import { cleanObject, useDebounce, useMount } from "../../utils";
 import * as qs from "qs";
 
 const apiUrl = process.env.REACT_APP_API_URL;
-{
-  /*npm start -> .env.development npm run build-> .env */
-}
+
 export const ProjectListScreen = () => {
   const [param, setParam] = useState({
     name: "",
@@ -15,30 +13,31 @@ export const ProjectListScreen = () => {
   });
   const [users, setUsers] = useState([]);
   const [list, setList] = useState([]);
+  const debouncedParam = useDebounce(param, 2000);
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(
       async (response) => {
         if (response.ok) {
           setList(await response.json());
         }
       }
     );
-  }, [param]);
+  }, [debouncedParam]);
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async (response) => {
       if (response.ok) {
         setUsers(await response.json());
       }
     });
-  }, [param]);
+  });
 
   return (
     <div>
-      <SearchPanel users={users} param={param} setParam={setParam} />{" "}
-      {/*react props*/}
+      <SearchPanel users={users} param={param} setParam={setParam} />
       <List users={users} list={list} />
     </div>
   );
 };
+
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export const isFalsy = (value) => (value === 0 ? false : !value);
 // 在一个函数里，改变传入的对象是不好的
 export const cleanObject = (object) => {
@@ -9,4 +11,21 @@ export const cleanObject = (object) => {
     }
   });
   return result;
+};
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    // 每次在value变化后设置一个定时器
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    // 每次在上一个useEffect处理完后才会运行，最后就只有最后一个存活下来了，就只会处理最后的输入
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+  return debouncedValue;
 };
